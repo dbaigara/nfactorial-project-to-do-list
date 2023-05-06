0a5d2b3eec750280bc8ed97864c4e298c973e2e6
dbaigara <dbaigara@cern.ch>
2023-05-06 23:26:12 +0300

    basic structure of the project is prepared

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import HEADER from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
+
   return (
-    <div className="App">
-      <p>First commit</p>
-     </div>
-  );
+    <div className='container mt-5'> 
+      <div className='mb-5'><HEADER /></div>
+      <div className='mb-5'><MainContent /></div>
+      <div className='pt-5'><Footer /></div>
+    </div>
+     );
 }
 
 export default App;
+
---
+++ src/img/index.js
@@ -0,0 +1,5 @@
+export const PLUS = require("./plus.png");
+export const DONE = require("./Done.png");
+export const MOVEBACK = require("./MoveBack.png");
+export const TRASH = require("./Trash.png");
+export const MORE = require("./More.png");
