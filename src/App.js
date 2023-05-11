0901836361803dddeff676bf8ae4e2fa2764ce37
dbaigara <dbaigara@cern.ch>
2023-05-11 14:17:00 +0300

    fix menu button with modal

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import './App.css';
-import HEADER from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
+import "./App.css";
+import { useState } from "react";
+import HEADER from "./components/Header/Header";
+import MainContent from "./components/MainContent/MainContent";
+import Footer from "./components/Footer/Footer";
 
 function App() {
 
+const [toDO, setToDo] = useState("");
+const [mainContent, setMainContent] = useState("");
+const [footer, setFooter] = useState("");
+
   return (
-    <div className='container mt-5'> 
-      <div className='mb-5'><HEADER /></div>
-      <div className='mb-5'><MainContent /></div>
-      <div className='pt-5'><Footer /></div>
+    <div className="container mt-5">
+      <div className="mb-5">
+        <HEADER />
+      </div>
+      <div className="mb-5 mt-5 pt-5">
+        <MainContent />
+      </div>
+      <div className="pt-5">
+        <Footer />
+      </div>
     </div>
-     );
+  );
 }
 
 export default App;
-
